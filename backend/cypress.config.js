1f8464a04e1dc2c1f14941049faa55f66306e9f3
Mauricio Robinson <marsjr97@gmail.com>
2023-01-28 01:42:45 -0500

    Deleting the nested backend folder

--- backend/cypress.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  e2e: {
-    baseUrl: 'http://localhost:8080/api/v1',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
-  },
-};
