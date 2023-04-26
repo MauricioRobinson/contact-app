ea02c8093f2efaec5a2e474bf23f2a5b9d86b5fa
Mauricio Robinson <marsjr97@gmail.com>
2023-04-26 18:14:54 -0400

    Migrating the signup ppage from pages to the app router

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,11 +1,11 @@
-import { LoginForm } from "@/components/LoginForm";
 import React from "react";
+import { SignupForm } from "@/components/SignupForm";
 
 const Page = () => {
   return (
     <section>
       <main className="max-w-screen-xl lg:max-w-screen-2xl px-8 mx-auto">
-        <LoginForm />
+        <SignupForm />
       </main>
     </section>
   );
--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -1,30 +1,43 @@
+"use client";
+
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { ChangeEventHandler, FormEventHandler, useState } from "react";
 import { Toast } from "@/components/Toast";
 import { ToastMessage } from "@/components/ToastMessage";
 import { CloseToast } from "@/components/CloseToast";
 import { Spinner } from "@/components/Spinner";
 import axios from "axios";
-import useAuth from "@/hooks/useAuth";
+// import useAuth from "@/hooks/useAuth";
 import { useSignup } from "@/hooks/useSignup";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
-const SignupForm = () => {
-  const router = useRouter();
-  const [send, setSend] = useState(false); //Controlls sending state
-  const [sendError, setSendError] = useState(false); //Controlls the behavior of the toast message
-  const [toast, setToast] = useState(false); //Controlls the open and close of the toast
-  const [signupData, setSignupData] = useState({
+type ISignup = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  age: number | null | undefined;
+  country: string | null | undefined;
+};
+
+const SignupForm = (): JSX.Element => {
+  // const router = useRouter();
+  // const [send, setSend] = useState(false); //Controlls sending state
+  // const [sendError, setSendError] = useState(false); //Controlls the behavior of the toast message
+  // const [toast, setToast] = useState(false); //Controlls the open and close of the toast
+  const [signupData, setSignupData] = useState<ISignup>({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
+    country: null,
+    age: null,
   });
-  const { signup, error, isLoading } = useSignup();
+  // const { signup, error, isLoading } = useSignup();
 
-  const { user, dispatch } = useAuth();
+  // const { user, dispatch } = useAuth();
 
-  const handleChange = (e) => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setSignupData((prevState) => {
       const { name, type, value, checked } = e.target;
 
@@ -35,9 +48,10 @@ const SignupForm = () => {
     });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    await signup(signupData);
+    // await signup(signupData);
+    console.log(signupData);
     // await fetchData();
   };
 
@@ -62,13 +76,13 @@ const SignupForm = () => {
       // setSend(false);
       // setSendError(false);
 
-      dispatch({
-        type: "LOGIN",
-        payload: {
-          token: response.data.token,
-          username: response.data.user.email,
-        },
-      });
+      // dispatch({
+      //   type: "LOGIN",
+      //   payload: {
+      //     token: response.data.token,
+      //     username: response.data.user.email,
+      //   },
+      // });
 
       // const interval = setTimeout(() => {
       //   router.push("/contacts");
@@ -157,35 +171,23 @@ const SignupForm = () => {
             <div className="mx-auto md:col-span-2 md:mr-0 lg:col-span-4">
               <button
                 type="submit"
-                disabled={isLoading}
                 className="rounded-md text-white bg-green-600 transition duration-500 ease-in-out hover:bg-green-700 hover:ring hover:ring-green-600 hover:ring-offset-2 hover:ring-offset-violet-600 px-4 py-1 font-bold mt-2">
-                {isLoading ? (
-                  <p className="w-full flex items-center justify-center gap-2">
-                    <span>
-                      <Spinner className="w-5 h-5 animate-spin fill-white stroke-yellow-500 stroke-2" />{" "}
-                    </span>
-                    <span>Signing up...</span>
-                  </p>
-                ) : (
-                  <p className="w-full flex items-center justify-center gap-2">
-                    <span>Signup</span>
-                  </p>
-                )}
+                Signup
               </button>
             </div>
-            {error && (
+            {/* {error && (
               <div className="mx-auto border rounded-md px-4 py-2 bg-red-400">
                 <p className="flex items-center gap-x-2">
                   <ExclamationTriangleIcon className="text-red-600 w-5 h-5" />{" "}
                   {error}
                 </p>
               </div>
-            )}
+            )} */}
           </form>
         </div>
       </div>
 
-      <Toast open={toast}>
+      {/* <Toast open={toast}>
         <CloseToast setToast={setToast} />
         {sendError ? (
           <ToastMessage
@@ -198,7 +200,7 @@ const SignupForm = () => {
             message="Account created successfully!"
           />
         )}
-      </Toast>
+      </Toast> */}
     </section>
   );
 };
